76f6e51f4a8f096217faff1cb546abd251f9f80f
Owlbeard <omeraltun96@gmail.com>
2023-03-18 03:07:42 +0300

    Version 1.0

---
+++ script.js
@@ -0,0 +1,70 @@
+const container = document.querySelector(".container");
+
+function gridMaker(num) {
+  for (let i = 1; i <= num ** 2; i++) {
+    let div = document.createElement("div");
+    container.appendChild(div).className = "cell";
+  }
+}
+
+function boxAdjust(num) {
+  let proportion = 672 / num - 2;
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].setAttribute("style", `height: ${proportion}px`)
+    cells[i].setAttribute("style", `width: ${proportion}px`)
+    };
+}
+
+function removeElementsByClass(className){
+  const elements = document.getElementsByClassName(className);
+  while(elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+gridMaker(16);
+
+const cells = Array.from(document.getElementsByClassName("cell"));
+
+boxAdjust(16);
+
+document.getElementById("generate").onclick = function() {
+  removeElementsByClass("cell");
+  let userChoice = prompt("Type your choice of number of squares per side for the new grid\n(Up to 100, no negatives.)");
+  let canvas = Number(userChoice);
+  if (canvas >= 0  && canvas <= 100) {
+    for (let i = 1; i <= canvas ** 2; i++) {
+      let div = document.createElement("div");
+      container.appendChild(div).className = "cell";
+    }
+    const cells = Array.from(document.getElementsByClassName("cell"));
+    let proportion = 672 / canvas - 2;
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].setAttribute("style", `height: ${proportion}px`)
+      cells[i].setAttribute("style", `width: ${proportion}px`)
+    };
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].addEventListener('mouseover', () => {
+        cells[i].classList.add('black');
+      });
+    }
+    document.getElementById("restart").onclick = function() {
+      for (let i = 0; i < cells.length; i++) {
+        cells[i].classList.remove('black');
+      }
+    };
+  } 
+}
+
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('mouseover', () => {
+    cells[i].classList.add('black');
+  });
+}
+
+document.getElementById("restart").onclick = function() {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].classList.remove('black');
+  }
+};
